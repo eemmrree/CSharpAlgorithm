79da1b123a13451a7253e92158c2920550a1f016
Emre <eemmrree0@outlook.com>
2021-10-18 20:59:28 +0300

    Example2 updated

---
+++ CSharpAlgorithm/2/Example2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgorithm._2
+{
+    class Example2
+    {
+        static void Main2(string[] args)
+        {
+
+        }
+    }
+}
